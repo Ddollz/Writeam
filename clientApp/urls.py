e65c024c915cf04fcbb16c464a95b76781bd33dc
Ddollz <44191473+Ddollz@users.noreply.github.com>
2021-09-07 10:10:42 +0800

    add user profile

--- clientApp/urls.py
+++ clientApp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.index, name="index"),
     path("faq", views.faq, name="faq"),
+    path("userprofile", views.profile, name="userprofile"),
     path("resume", views.resume, name="resume"),
 
 ]
