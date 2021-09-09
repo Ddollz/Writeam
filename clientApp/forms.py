fef9a2ffa54d0c91119e74f7c6db72c63af92286
Ddollz <44191473+Ddollz@users.noreply.github.com>
2021-09-09 11:19:12 +0800

    add personal detail required

--- clientApp/forms.py
+++ clientApp/forms.py
@@ -6,31 +6,31 @@ from .models import personalDetails, employmentHistory, education, skill, link, 
 class personalDetailsForm(ModelForm):
     profile_image = forms.ImageField(label="", widget=forms.FileInput(
         attrs={'type': 'file', 'id': 'mediaFile'}))
-    jobTitle = forms.CharField(label="", required=False, widget=forms.TextInput(
+    jobTitle = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'jobTitle'}))
-    fname = forms.CharField(label="", required=False, widget=forms.TextInput(
+    fname = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'fname'}))
-    lname = forms.CharField(label="", required=False, widget=forms.TextInput(
+    lname = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'lname'}))
-    email = forms.CharField(label="", required=False, widget=forms.TextInput(
+    email = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'email', 'class': 'form-control', 'id': 'email'}))
-    phone = forms.CharField(label="", required=False, widget=forms.TextInput(
+    phone = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'phone'}))
-    country = forms.CharField(label="", required=False, widget=forms.TextInput(
+    country = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'country'}))
-    city = forms.CharField(label="", required=False, widget=forms.TextInput(
+    city = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'city'}))
-    address = forms.CharField(label="", required=False, widget=forms.TextInput(
+    address = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'address'}))
-    birthplace = forms.CharField(label="", required=False, widget=forms.TextInput(
+    birthplace = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'pdate'}))
-    birthdate = forms.CharField(label="", required=False, widget=forms.TextInput(
+    birthdate = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'bdate'}))
-    nationality = forms.CharField(label="", required=False, widget=forms.TextInput(
+    nationality = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'national'}))
-    profSummary = forms.CharField(label="", required=False, widget=forms.Textarea(
+    profSummary = forms.CharField(label="", required=True, widget=forms.Textarea(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'summaryEditor'}))
-    postalcode = forms.CharField(label="", required=False, widget=forms.TextInput(
+    postalcode = forms.CharField(label="", required=True, widget=forms.TextInput(
         attrs={'type': 'text', 'class': 'form-control', 'id': 'postal'}))
 
     class Meta:
